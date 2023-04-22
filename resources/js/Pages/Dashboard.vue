848475ddac2edd6f318cd81433c7cbd14958dacc
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2023-04-22 13:25:56 +0300

    Updated chart

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -43,12 +43,12 @@
         <div
              class="relative sm:flex sm:justify-center sm:items-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
-            <div class="mx-auto p-6 lg:p-8 w-screen">
+            <div class="min-w-7xl mx-auto p-6 lg:p-8">
 
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <a href="#"
-                           class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                           class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-96">
 
                             <div class="w-full">
                                 <h1 class="mt-6 text-4xl font-thin text-gray-900 dark:text-white mb-10">Tasks</h1>
@@ -75,7 +75,7 @@
                         </a>
 
                         <a href="#"
-                           class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                           class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-96">
 
                             <div class="w-full">
                                 <h2 class="mt-6 text-4xl font-semibold text-gray-900 dark:text-white mb-10">Users</h2>
@@ -97,8 +97,7 @@
                            class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
 
                             <div class="w-full">
-                                <h1 class="mt-6 text-4xl font-thin text-gray-900 dark:text-white mb-10">Tasks</h1>
-
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white mb-10">Tasks</h2>
                                 <div class="flex justify-between dark:text-white gap-2">
                                     <canvas id="taskChart"></canvas>
                                 </div>
@@ -109,8 +108,7 @@
                            class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
 
                             <div class="w-full">
-                                <h1 class="mt-6 text-4xl font-thin text-gray-900 dark:text-white mb-10">Users</h1>
-
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white mb-10">Users</h2>
                                 <div class="flex justify-between dark:text-white gap-2">
                                     <canvas id="userChart"></canvas>
                                 </div>
